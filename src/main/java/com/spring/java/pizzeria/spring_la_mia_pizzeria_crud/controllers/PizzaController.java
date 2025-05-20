32022e76eebad65183f5b6e60e3b224ba710224e
alessandro-sartini <ale.sartini.98@gmail.com>
2025-05-20 13:05:22 +0200

    Implement Pizza and Home controllers; update application properties and SQL data; add HTML templates for home and pizza listing

---
+++ src/main/java/com/spring/java/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -0,0 +1,37 @@
+package com.spring.java.pizzeria.spring_la_mia_pizzeria_crud.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.spring.java.pizzeria.spring_la_mia_pizzeria_crud.model.Pizza;
+import com.spring.java.pizzeria.spring_la_mia_pizzeria_crud.repo.PizzaRepository;
+
+
+@Controller
+@RequestMapping("/pizze")
+public class PizzaController {
+
+    @Autowired
+    private PizzaRepository repository;
+
+    @GetMapping
+    public String index(Model model) {
+
+        List<Pizza> pizzaList= repository.findAll();
+
+        model.addAttribute("pizzaList", pizzaList);
+        for (Pizza pizza : pizzaList) {
+            
+            System.out.println(pizza.getName());
+        }
+        return "pizzas/index";
+    }
+    
+
+
+}
