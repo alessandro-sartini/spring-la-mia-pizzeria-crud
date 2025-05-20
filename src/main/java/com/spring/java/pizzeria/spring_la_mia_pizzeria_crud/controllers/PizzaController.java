5abc96252360598c60963fc80095af5e17b3bd5c
alessandro-sartini <ale.sartini.98@gmail.com>
2025-05-20 16:13:05 +0200

    Refactor Home and Pizza controllers; update HTML templates for navigation and pizza display

--- src/main/java/com/spring/java/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/com/spring/java/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -1,12 +1,14 @@
 package com.spring.java.pizzeria.spring_la_mia_pizzeria_crud.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.spring.java.pizzeria.spring_la_mia_pizzeria_crud.model.Pizza;
 import com.spring.java.pizzeria.spring_la_mia_pizzeria_crud.repo.PizzaRepository;
@@ -27,5 +29,17 @@ public class PizzaController {
 
         return "pizzas/index";
     }
+
+    @GetMapping("/{id}")
+    public String show(Model model, @PathVariable("id")int id) {
+
+        
+        Optional<Pizza> result = repository.findById(id);
+
+        model.addAttribute("pizza", result.orElse(null));
+        return "pizzas/show";
+    }
+
+
 
 }
