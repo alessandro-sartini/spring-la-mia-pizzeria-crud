85475ef2168720cedae0f94f9b9d6a53538e12b0
alessandro-sartini <ale.sartini.98@gmail.com>
2025-05-20 11:36:09 +0200

    Refactor Pizza entity and update application properties; add initial SQL data for pizza entries

--- src/main/java/com/spring/java/pizzeria/spring_la_mia_pizzeria_crud/model/Pizza.java
+++ src/main/java/com/spring/java/pizzeria/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -1,8 +1,6 @@
 package com.spring.java.pizzeria.spring_la_mia_pizzeria_crud.model;
 
 import java.math.BigDecimal;
-
-import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
